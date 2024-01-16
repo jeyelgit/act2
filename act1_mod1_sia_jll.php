9f988fca1826b25e48577480a573ade8f0756269
jeyelgit <148886313+jeyelgit@users.noreply.github.com>
2024-01-16 14:49:23 +0800

    Add files via upload

---
+++ act1_mod1_sia_jll.php
@@ -0,0 +1,25 @@
+<?php
+
+    echo "<b>PERSONAL INFORMATION</b> <br>";
+    echo "Name: John Llyod Lugtu <br>";
+    echo "Birthday: September 2, 2003 <br>";
+    echo "Address: Ascomo, Guagua, Pampanga <br>";
+
+    echo "<br>";
+    
+    echo "<b> EDUCATIONAL BACKGROUND </b> <br>";
+    echo "Primary: Ascomo, Elementary, School <br>";
+    echo "Secondary: West Central College <br>";
+    echo "Teartiary: Mary the Queen College of Pampanga <br>";
+    echo "Course: Bachelor of Science in Information Technology <br>";
+
+    echo "<br>";
+
+    echo "Programmed by: John Llyod Lugtu";
+
+
+
+
+    
+
+?>
---
+++ mp1_mod1_sia_jll.php
@@ -0,0 +1,50 @@
+<?php
+
+echo "<b>PERSONAL INFORMATION</b><br>";
+echo "Name: John Llyod R. Lugtu<br>";
+echo "Birthday: September 02 2003<br>";
+echo "Gender: Male<br>";
+echo "Home Address: Ascomo, Guagua, Pampanga<br>";
+echo "Email Address: lloydies02@gmail.com<br><br>";
+
+echo "<b>FAMILY BACKROUND</b><br>";
+echo "father's Name: lodelino Lugtu<br>";
+echo "Mother's Name: Analyn Roman<br>";
+echo "Sibling: Adriane Lugtu<br><br>";
+
+echo "<b>EDUCATIONAL BACKGROUND</b>";
+echo "Primary School: Ascomo Elemntary School<br>";
+echo "Date Graduated: 2015<br>";
+echo "Junior High School: Pulungmasle High School<br>";
+echo "Date Graduated:2019<br>";
+echo "Senior High School: West Central College<br>";
+echo "Date Graduated: 2021";
+echo "teriary School: Mary the Queen College (Pampanga)<br>";
+echo "Course: Bachelor of Science in Information Technology<br><br>";
+
+echo "<b>FAVORITE FOODS</b><br>";
+echo "1. Adobong Manok<br>";
+echo "2. Chicken Nuggets<br>";
+echo "3. Sisig<br>";
+echo "4. Daing na bangus<br>";
+echo "5. Beef Pares<br><br>";
+
+echo "FAVORITE MOVIES<br>";
+echo "1. Encanto<br>";
+echo "2. The Whale<br>";
+echo "3. Aeon Flux<br>";
+echo "4. Guardians of the Galaxy<br>";
+echo "5. Love you long time<br><br>";
+
+echo "FAVORITE SONGS<br>";
+echo "1. MANA<br>";
+echo "2. GENTO<br>";
+echo "3. Araw-araw<br>";
+echo "4. Paninindigan kita<br>";
+echo "5. WYAT<br><br>";
+
+echo "Programmed by: John Llyod Lugtu";
+
+
+
+?>
